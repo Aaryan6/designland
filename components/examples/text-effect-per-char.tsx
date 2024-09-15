ba90ba433e3e6d4de85d328bcfacc80779846d24
Aaryan6 <aaryanpatel683@gmail.com>
2024-09-15 22:13:16 +0530

    remove files from example

--- components/examples/text-effect-per-char.tsx
+++
@@ -1,9 +0,0 @@
-import { TextEffect } from '../core/text-effect';
-
-export function TextEffectPerChar() {
-  return (
-    <TextEffect per='char' preset='fade'>
-      Animate your ideas with motion-primitives
-    </TextEffect>
-  );
-}
